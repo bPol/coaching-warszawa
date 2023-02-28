1a12d0481529af35d254f5c90c40e0908938c500
Bern <b@bernhard-huber.eu>
2023-02-28 08:09:41 +0100

    Initial project setup

---
+++ src/components/CoachingWarszawa.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="d-flex align-center text-center fill-height">
+      <v-img
+        contain
+        height="300"
+        src="@/assets/logo.svg"
+      />
+
+      <div class="text-body-2 font-weight-light mb-n1">Zapraszam do</div>
+
+      <h1 class="text-h2 font-weight-bold">Coaching Warszawa</h1>
+
+      <div class="py-14" />
+      <v-row class="d-flex align-center justify-center">
+        <v-col cols="auto"><p class="text-justify">Jestem HR-owcem z 15-letnim doświadczeniem pracy z ludźmi. Jakiś czas temu postanowiłam skupić się bardziej na obszarze life coachingu. Z wykształcenia jestem pedagogiem i psychologiem, a obszary rozwojowe, podnoszące samoświadomość były mi zawsze bliskie; zwłaszcza samorozwój kobiet oraz wspieranie ich w podążaniu własną drogą i odkrywanie siebie. Specjalizuję się w obszarach coachingu relacji, life coachingu, coachingu kariery i biznesowym. Pracuję w nastawieniu na cele Klienta, skupiam się na jego mocnych stronach, możliwościach i potrzebach.</p></v-col>
+      </v-row>
+      <v-row class="d-flex align-center justify-center">
+
+        <v-col cols="auto">
+          <v-btn
+            color="primary"
+            href="https://next.vuetifyjs.com/introduction/why-vuetify/#feature-guides"
+            min-width="228"
+            rel="noopener noreferrer"
+            size="x-large"
+            target="_blank"
+            variant="flat"
+          >
+            <v-icon
+              icon="mdi-speedometer"
+              size="large"
+              start
+            />
+
+            Rozpocznij coaching
+          </v-btn>
+        </v-col>
+
+      </v-row>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup>
+  //
+</script>
